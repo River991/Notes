97fa48e8b4bf77bb24dd853a75d2fd1911e92c94
River Phillips <riverphillips1@gmail.com>
2017-11-23 11:53:39 +0000

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
+
     assert_response :success
+    
+    assert_template layout: 'application'
+    
+    assert_select 'title', 'My Notes'
+    assert_select 'h1', 'My Notes'
+    assert_select 'p', 'Welcome to my notes site!'
   end
 
 end
